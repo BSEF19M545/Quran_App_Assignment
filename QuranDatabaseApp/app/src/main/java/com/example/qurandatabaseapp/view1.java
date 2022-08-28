f0b515d1ac89a96287fbcbc22bf0afbf49385f6e
Abdul wahab <bsef19m545@pucit.edu.pk>
2022-08-28 19:23:01 +0500

    Quran App Assignment search surah

--- QuranDatabaseApp/app/src/main/java/com/example/qurandatabaseapp/view1.java
+++ QuranDatabaseApp/app/src/main/java/com/example/qurandatabaseapp/view1.java
@@ -4,34 +4,69 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.Locale;
+import java.util.concurrent.TimeUnit;
 
 public class view1 extends AppCompatActivity {
-
+    ArrayList<String> surahIds;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view1);
         DBHelper db=new DBHelper(this);
-        ArrayList<String> surahIds=db.getSurahIds();
+        surahIds=db.getSurahIds();
         ListView view1ListView=findViewById(R.id.view1ListView);
 
-        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>
                 (this, android.R.layout.simple_list_item_1, surahIds);
 
         view1ListView.setAdapter(arrayAdapter);
-        view1ListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        view1ListView.setOnItemClickListener((adapterView, view, i, l) -> {
+            Intent intent = new Intent(view1.this, view2.class);
+            intent.putExtra("surahId",i);
+            startActivity(intent);
+        });
+
+        EditText searchBar=findViewById(R.id.searchBar);
+
+        searchBar.addTextChangedListener(new TextWatcher()
+        {
             @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                Intent intent = new Intent(view1.this, view2.class);
-                intent.putExtra("surahId",i);
-                startActivity(intent);
+            public void afterTextChanged(Editable mEdit)
+            {
+                ArrayList<String> rtn=new ArrayList<>();
+                for(int i=0;i<surahIds.size();i++)
+                {
+                    if (surahIds.get(i).toLowerCase(Locale.ROOT).contains(mEdit.toString().toLowerCase(Locale.ROOT)))
+                    {
+                        rtn.add(surahIds.get(i));
+                    }
+                }
+                if(rtn.size()>0)
+                {
+                    ArrayAdapter<String> adapter = new ArrayAdapter<>(view1.this, android.R.layout.simple_list_item_1, rtn);
+                    view1ListView.setAdapter(adapter);
+                }
+                else{
+                      Toast.makeText(getApplicationContext(),"No data found",Toast.LENGTH_SHORT).show();
+                }
+
             }
+
+            public void beforeTextChanged(CharSequence s, int start, int count, int after){}
+
+            public void onTextChanged(CharSequence s, int start, int before, int count){}
         });
     }
 }
